e85695c788e2d091246654056aa3ddbde3ddc6ce
Ariel Eduardo Pabón Bolaños <lyca2206@gmail.com>
2025-04-14 10:20:16 -0500

    Initialized project

---
+++ src/main/java/com/lyca2206/backend_personal_fitness_tracker/BackendPersonalFitnessTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.lyca2206.backend_personal_fitness_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPersonalFitnessTrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPersonalFitnessTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lyca2206/backend_personal_fitness_tracker/BackendPersonalFitnessTrackerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lyca2206.backend_personal_fitness_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPersonalFitnessTrackerApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
