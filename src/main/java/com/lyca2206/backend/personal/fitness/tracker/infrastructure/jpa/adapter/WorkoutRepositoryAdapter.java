a848be0976774ee77bb5fc8a1ca29a2a22de4ec9
Ariel Eduardo Pabón Bolaños <lyca2206@gmail.com>
2025-04-24 23:01:59 -0500

    Fully implemented Workout Creation, Refactored REST Request

--- src/main/java/com/lyca2206/backend/personal/fitness/tracker/infrastructure/jpa/adapter/WorkoutRepositoryAdapter.java
+++ src/main/java/com/lyca2206/backend/personal/fitness/tracker/infrastructure/jpa/adapter/WorkoutRepositoryAdapter.java
@@ -2,8 +2,11 @@ package com.lyca2206.backend.personal.fitness.tracker.infrastructure.jpa.adapter
 
 import com.lyca2206.backend.personal.fitness.tracker.application.domain.model.Workout;
 import com.lyca2206.backend.personal.fitness.tracker.application.port.spi.WorkoutRepository;
+import com.lyca2206.backend.personal.fitness.tracker.infrastructure.jpa.entity.ExerciseEntity;
 import com.lyca2206.backend.personal.fitness.tracker.infrastructure.jpa.entity.WorkoutEntity;
 import com.lyca2206.backend.personal.fitness.tracker.infrastructure.jpa.mapper.WorkoutEntityMapper;
+import com.lyca2206.backend.personal.fitness.tracker.infrastructure.jpa.repository.ExerciseRepositoryJPA;
+import com.lyca2206.backend.personal.fitness.tracker.infrastructure.jpa.repository.WorkoutExerciseRepositoryJPA;
 import com.lyca2206.backend.personal.fitness.tracker.infrastructure.jpa.repository.WorkoutRepositoryJPA;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,6 +17,8 @@ import java.util.List;
 @Component
 public class WorkoutRepositoryAdapter implements WorkoutRepository {
     private final WorkoutRepositoryJPA workoutRepositoryJPA;
+    private final WorkoutExerciseRepositoryJPA workoutExerciseRepositoryJPA;
+    private final ExerciseRepositoryJPA exerciseRepositoryJPA;
     private final WorkoutEntityMapper workoutEntityMapper;
 
     @Override
@@ -26,7 +31,16 @@ public class WorkoutRepositoryAdapter implements WorkoutRepository {
     @Override
     public void save(Workout workout) {
         WorkoutEntity workoutEntity = workoutEntityMapper.workoutToWorkoutEntity(workout);
-        workoutRepositoryJPA.save(workoutEntity);
+        WorkoutEntity savedWorkout = workoutRepositoryJPA.save(workoutEntity);
+
+        savedWorkout.getWorkoutExercises().forEach(workoutExerciseEntity -> {
+            workoutExerciseEntity.setWorkout(savedWorkout);
+
+            ExerciseEntity savedExercise = exerciseRepositoryJPA.findByName(workoutExerciseEntity.getExercise().getName());
+            workoutExerciseEntity.setExercise(savedExercise);
+
+            workoutExerciseRepositoryJPA.save(workoutExerciseEntity);
+        });
     }
 
     @Override
